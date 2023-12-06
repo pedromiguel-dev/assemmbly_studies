ff9f85b9cbfc6f9755a3408c2d5a2edbb8cd4573
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2023-12-06 10:18:52 -0300

    float: adding, printing and using

---
+++ float.asm
@@ -0,0 +1,15 @@
+section .data
+		; p $xmm0.v4_float[0]
+		x dd 3.14
+		y dd 2.1
+		
+section .text
+global _start
+
+_start:
+		movss		xmm0,	[x]
+		movss		xmm1,	[y]
+		addss		xmm0,	xmm1
+		
+		mov 		ebx, 	1
+		int			80h
